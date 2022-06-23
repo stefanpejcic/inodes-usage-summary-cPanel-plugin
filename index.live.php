3acffb10f3a9660db0154e380536b58ed6077a2a
Stefan Pejcic <pejcha1994@gmail.com>
2022-06-23 16:41:29 +0200

    Create index.live.php

---
+++ index.live.php
@@ -0,0 +1,59 @@
+<?php
+require '/usr/local/cpanel/php/cpanel.php';
+
+class Account
+{
+    public static function name($cpanel)
+    {
+        return $cpanel->cpanelprint('$user');
+    }
+}
+
+
+$cpanel = new CPANEL();
+print $cpanel->header( "Inodes Usage Summary" );
+$accountName = Account::name($cpanel);
+$path = '/home/' . $accountName;
+?>  
+
+<link rel="stylesheet" type="text/css" href="assets/css/main.css" media="screen" />
+
+<div class="container" style="width: auto">
+        <div v-model=""></div>
+        <div class="content">
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <ul class="nav navbar-nav">
+<li><a href="#" style="cursor:default;text-decoratio:none;">Inode usage for: <b><?php echo $path;?></b></a></li>                        
+ </ul>
+                </div>
+            </nav>
+
+
+<?php
+
+
+//Store the output of the executed command in an array
+
+exec('cd $path && /usr/local/cpanel/base/frontend/paper_lantern/inodes/./inodes', $output, $status);
+
+
+//Print all return values of the executed command as array
+
+
+
+echo "<br/>";
+echo "<pre>";
+//Print the output of the executed command in each line
+
+foreach($output as $value){
+        echo $value."<br />";
+}
+
+//Print the return status of the executed command
+
+echo "</pre>";
+echo $cpanel->footer();
+$cpanel->end();
+
+
